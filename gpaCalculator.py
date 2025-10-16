a83f3e42751c422ff8a1cbf1be3cb2383106911b
oof15642 <59772241+Fooot-Code@users.noreply.github.com>
2025-10-15 20:44:00 -0600

    added first semester and second semester, as well as partial maintain or improve gpa system.

--- gpaCalculator.py
+++ gpaCalculator.py
@@ -33,31 +33,69 @@ for gradeCount in range(1, gradeAmount+1): # +1 allows for input statements to b
 gpa = calculateGPA(grades)
 print("Calculating...")
 sleep(2)
-print(f"Your current GPA is {gpa}!")
+print(f"Your current GPA is {round(gpa, 2)}!")
 sleep(3)
-print(f"Your GPA for your first two classes is {calculateGPA(grades[:2])}")
+gradeAmount = len(grades)
+
+# If odd, first semester gets the extra class
+mid = (gradeAmount + 1) // 2  
+
+firstSemesterGPA = calculateGPA(grades[:mid])
+lastSemesterGPA = calculateGPA(grades[mid:])
+
+consistent4 = False if firstSemesterGPA != 4 and lastSemesterGPA != 4 else True # Has a 4.0 through both semesters.
+
+
+print(f"Your GPA for your first semester is {round(firstSemesterGPA, 2)}!")
 sleep(3)
-print(f"Your GPA for your last two classes is {calculateGPA(grades[-2:])}")
+print(f"Your GPA for your last semester is {round(lastSemesterGPA, 2)}!")
+
+if firstSemesterGPA > lastSemesterGPA:
+    print(f"You didn't improve your GPA from last semester! You must improve it now!")
+elif lastSemesterGPA > firstSemesterGPA:
+    print(f"Good job, your GPA has improved between semesters!")
+elif consistent4:
+    print(f"WOW! You are really good at school, great job maintaining a 4.0 throughout both semesters!")
+else: # Got the same grade they did last semester
+    print(f"You have not improved on your GPA yet.")
+
+if not consistent4:
+    maintainOrImprove = input("Would you like to maintain or improve your GPA?\n>")
+    while maintainOrImprove.lower() not in ["maintain", "improve"]:
+        print('Please type either "maintain" or "improve"')
+        maintainOrImprove = input("> ")
+    if maintainOrImprove.lower() == "maintain":
+        if gpa < 2:
+            print(f"Okay, but I would consider improving your GPA, a {round(gpa, 1)} is not very good.") # Rounds to 1 decimal for emphasis
+        else:
+            print(f"Sounds good, good work on your GPA!")
+
+    elif maintainOrImprove.lower() == "improve":
+        while True:
+            try:
+                targetGPA = float(input("Good choice! What is your target GPA? \n> "))
+                break
+            except:
+                print("Please enter a number with valid characters")
+        
+        while targetGPA > 4.0 or targetGPA < 0:
+            print("Please do not enter a negative number or a number above 4!")
+            targetGPA = input("> ")
+
+        newGrades = grades.copy()
+        lowestGradeIndex = newGrades.index(min(newGrades))
+        newGrades[lowestGradeIndex] = 4.0 # Selects the lowest grade in the grades, and makes it a 4.0
+        print(f"If you were to improve your grade in Class {lowestGradeIndex + 1}, your grades would look like this:")
+        
+        for classNumber, grade in enumerate(newGrades, 1):
+            print(f"Class {classNumber}: {grade}")
 
 
-# grades = input("Please enter your grades, in the format grade1, grade2, grade3, etc. Please enter them in a 4.0 format\n> ")
-# grades = grades.split(",")
-
-# while True:
-#     try:
-#         grades = [float(grade) for grade in grades]
-#         break
-#     except:
-#         print("Please enter correct values for each grade. Make sure they have no letters in them")
-#         grades = input("> ")
-#         grades = grades.split(",")
-
-# validGrades = True if all([True for grade in grades if abs(grade) == grade and (grade > 0 and grade < 4)]) else False
-# while not validGrades:
-#     print("You either have negative values in your grade list or your grades exceeed the 4 point scale. Please enter them again")
-#     grades = input("> ")
-#     grades = grades.split(",")
-#     validGrades = True if all([True for grade in grades if abs(grade) == grade and (grade > 0 and grade < 4)]) else False
+        
 
 
-# gpa = calculateGPA(grades)
+
+
+
+
+
